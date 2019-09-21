8ddbe8df6cb2381f7d31628ff4a53486a3dbe1f8
Viviana Laura Ponce <viviana.laura@unir.net>
2019-09-21 20:51:45 +0200

    Counter Sample of double databinding

--- curso01/curso01/src/app/app.component.ts
+++ curso01/curso01/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TimeInfo } from './user-profile/user-profile.component';
 
 @Component({
@@ -6,11 +6,30 @@ import { TimeInfo } from './user-profile/user-profile.component';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'curso01';
   user='Viviana';
   role="Admin";
+
+  
+  valor1=1;
+  valor2=1;
+  suma=0;
+  ngOnInit(){
+    this.calcularSuma();
+  }
   onLogout(evenInfo:TimeInfo):void{
     console.log('El usuario '+evenInfo.name + 'hizo logout a horas '+ evenInfo.timestamp);
+  }
+  valorCambio1(value:number):void{
+    this.valor1=value;
+    this.calcularSuma();
+  }
+  valorCambio2(value:number):void{
+    this.valor2=value;
+    this.calcularSuma();
+  }
+  calcularSuma(){
+    this.suma=this.valor1+this.valor2;
   }
 }
