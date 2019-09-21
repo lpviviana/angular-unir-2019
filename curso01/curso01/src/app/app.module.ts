26032c7856e16f668996846d8f9f77d66fdf7713
Viviana Laura Ponce <viviana.laura@unir.net>
2019-09-22 00:19:27 +0200

    Sample The Weather (services+ components)

--- curso01/curso01/src/app/app.module.ts
+++ curso01/curso01/src/app/app.module.ts
@@ -6,19 +6,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { CounterComponent } from './counter/counter.component';
+import { LoggerService } from './services/logger.service';
+import { Logger2Service } from './services/logger2.service';
+import { ListCityComponent } from './list-city/list-city.component';
+import { CityComponent } from './city/city.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserProfileComponent,
-    CounterComponent
+    CounterComponent,
+    ListCityComponent,
+    CityComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule
   ],
-  providers: [],
+  providers: [ 
+
+      {provide:LoggerService,useClass:Logger2Service}  
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
